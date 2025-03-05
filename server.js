d22ad77dfaf16e6b9c575501dab39dbe5e6e1c63
luminchik <luminartur@gmail.com>
2025-03-05 18:47:42 +0400

    Исправление проблемы с занятым портом, автоматический выбор порта

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(express.json());
 // Создаем клиент PostgreSQL вместо SQLite подключения
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
 });
 
 // Конфигурация DiscordStrategy
